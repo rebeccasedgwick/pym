4381fa18aabfcd9235d0f4a719cf0f68218e2807
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-11-12 10:52:33 +0000

    Adds test for #div raising error if div(0)

--- factorial/myfactorial/factorial_test.py
+++ factorial/myfactorial/factorial_test.py
@@ -1,5 +1,5 @@
 import unittest
-from factorial import factorial
+from factorial import factorial, div
 
 
 class TestFactorial(unittest.TestCase):
@@ -7,6 +7,12 @@ class TestFactorial(unittest.TestCase):
     def test_fact(self):
         res = factorial(5)
         self.assertEqual(res, 120)
+
+    def test_error(self):
+        """
+        Test if exception raised due to runtime error: division by 0
+        """
+        self.assertRaises(ZeroDivisionError, div, 0)
 
 
 if __name__ == '__main__':
